ca30de620d8d3152070da8639216d1c803f6d5e9
h-shiratsuki <shiratsuki2125@gmail.com>
2019-02-14 16:26:24 +0900

    Fixed not to put parentheses in title when type is empty

--- index.js
+++ index.js
@@ -138,7 +138,10 @@ const csvPath = path.join(csvDir, 'schedule.csv');
     const startMoment = moment(new Date(line[0] + ' ' + startTime));
     const endMoment = moment(new Date(line[2] + ' ' + endTime));
     
-    const summary = `[${line[4]}] ${line[5]}`;
+    let summary = `${line[5]}`;
+    if (line[4] !== '') {
+      summary = `[${line[4]}] ` + summary;
+    }
     const location = line[8];
 
     let start, end;
